c04e05c0c6f347d09a7fdcaba4c7a0ac6acfd5f8
Andrey Skoryk <asxcandrew@gmail.com>
2017-10-09 13:45:05 +0300

    refactored file reading

--- src/commands/index.rb
+++ src/commands/index.rb
@@ -1,16 +1,19 @@
 class Commands::Index < Commands::Base
   def execute
     Dir[Dir.pwd + '/raw/*'].each do |file|
-      Console.print(file)
-
       file = File.open(file)
 
-      if file.external_encoding.name == 'UTF-8'
+      begin
         file.read.gsub(/\r\n?/, "\n").each_line do |line|
           line.split(/\W+/).each do |word|
             Storage.insert(word.downcase, File.basename(file)) if word.length > 1
           end
         end
+        Console.print(file.path)
+      rescue
+        Console.print("Something wrong occurred during reading file: #{file.path}")
+      ensure
+        file.close unless file.nil?
       end
     end
     Console.print_empty_line
